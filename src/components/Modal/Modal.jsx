d60ca3dbea984c8b61ef05c2b560d449b8adcbc4
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2023-01-10 15:44:13 +0200

    modified:   src/components/Modal/Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { PropTypes } from 'prop-types';
@@ -6,17 +7,17 @@ import css from './Modal.module.css';
 const port = document.querySelector('#modal-portal');
 
 const Modal = ({ onClose, item }) => {
+  const handlerKey = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
   useEffect(() => {
-    const handlerKey = e => {
-      if (e.code === 'Escape') {
-        onClose();
-      }
-    };
     window.addEventListener('keydown', handlerKey);
     return () => {
       window.removeEventListener('keydown', handlerKey);
     };
-  }, []);
+  }, [handlerKey]);
 
   const handlerOverlay = e => {
     if (e.currentTarget === e.target) {
