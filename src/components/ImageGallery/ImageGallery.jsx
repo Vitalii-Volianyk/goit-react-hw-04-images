679eaa810af88a4c53744354403b74fe1af741c6
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2023-01-10 15:06:00 +0200

    modified:   src/components/ImageGallery/ImageGallery.jsx 	deleted:    src/components/ImageGallery/ImageGallery1.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,5 @@
 import { fetchImage } from 'services/api';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { PropTypes } from 'prop-types';
 import ImageGalleryItem from './ImageGalleryItem';
 import Modal from 'components/Modal';
@@ -9,7 +9,7 @@ import EmptyView from 'components/EmptyView';
 import ErrorView from 'components/ErrorView';
 import css from './ImageGallery.module.css';
 
-const status = {
+const STATUS = {
   IDLE: 'idle',
   PENDING: 'pending',
   RESOLVE: 'resolve',
@@ -17,130 +17,109 @@ const status = {
 };
 const controller = new AbortController();
 const signal = controller.signal;
-class ImageGallery extends Component {
-  state = {
-    imageList: null,
-    errorMessage: null,
-    curStatus: status.IDLE,
-    currentPage: 1,
-    showModal: false,
-    id: null,
-    totalHits: 0,
-  };
-  toggleModal = () => {
-    this.setState(prevState => {
-      return {
-        showModal: !prevState.showModal,
-      };
-    });
-  };
-  componentDidUpdate(
-    { searchParam: prevSearchParam },
-    { currentPage: prevCurrentPage }
-  ) {
-    const { searchParam } = this.props;
-    const { currentPage } = this.state;
 
-    if (prevSearchParam !== searchParam) {
-      this.getImages(searchParam, currentPage, true);
+const ImageGallery = ({ searchParam }) => {
+  const [imageList, setImageList] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [curStatus, setCurStatus] = useState(STATUS.IDLE);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [id, setId] = useState(null);
+  const [totalHits, setTotalHits] = useState(0);
+
+  const toggleModal = () => {
+    setShowModal(prevState => !prevState);
+  };
+  const loadModal = id => {
+    setId(id);
+    toggleModal();
+  };
+  useEffect(() => {
+    if (searchParam) {
+      setCurrentPage(1);
+      getImages(1, true);
     }
-    if (prevCurrentPage < currentPage) {
-      this.getImages(searchParam, currentPage);
+  }, [searchParam]);
+
+  useEffect(() => {
+    if (currentPage > 1) {
+      getImages(currentPage);
     }
-  }
-  componentWillUnmount() {
-    controller.abort();
-  }
-  getImages(search, page, statusShow = false) {
-    if (statusShow) {
-      this.setState({ curStatus: status.PENDING, currentPage: 1 });
+  }, [currentPage]);
+
+  const getImages = (page, notif = false) => {
+    if (!searchParam) {
+      return;
     }
-    fetchImage(search, page, signal)
+    if (notif) {
+      setCurStatus(STATUS.PENDING);
+    }
+    fetchImage(searchParam, page, signal)
       .then(response => {
         if (response.ok) {
           return response.json();
         }
-        return new Error('Image not found.Try again');
+        return new Error('Image not found. Try again');
       })
       .then(response => {
-        this.setState(({ imageList }) => {
-          if (page > 1) {
-            return {
-              imageList: [...imageList, ...response.hits],
-              curStatus: status.RESOLVE,
-            };
+        if (response.totalHits < 1) {
+          setCurStatus(STATUS.REJECT);
+          setErrorMessage('Image not found. Try again with new request');
+          return new Error('Image not found. Try again with new request');
+        }
+        setImageList(prevList => {
+          if (notif) {
+            setTotalHits(response.totalHits);
+            return response.hits;
           }
-          return {
-            imageList: response.hits,
-            curStatus: status.RESOLVE,
-            totalHits: response.totalHits,
-          };
+          return [...prevList, ...response.hits];
         });
+        setCurStatus(STATUS.RESOLVE);
       })
       .catch(error => {
-        this.setState({ curStatus: status.REJECT, errorMessage: error });
+        console.log(error);
+        setCurStatus(STATUS.REJECT);
+        setErrorMessage(error.message);
       });
+  };
+  const handleLoadMore = () => {
+    setCurrentPage(prevPage => prevPage + 1);
+  };
+  const reloadRequest = () => {
+    getImages(true);
+  };
+  if (curStatus === STATUS.IDLE) {
+    return <EmptyView />;
   }
-  showModal = id => {
-    this.setState({ showModal: true, id });
-  };
-  handleLoadMore = () => {
-    this.setState(prevState => {
-      return { currentPage: prevState.currentPage + 1 };
-    });
-  };
-  reloadRequest = () => {
-    const { searchParam } = this.props;
-    const { currentPage } = this.state;
+  if (curStatus === STATUS.PENDING) {
+    return <Loader />;
+  }
+  if (curStatus === STATUS.RESOLVE) {
+    const classes = showModal
+      ? css.container + ' ' + css.no_scroll
+      : css.container;
+    return (
+      <div className={classes}>
+        <ul className={css.gallery}>
+          {imageList.map((el, index) => (
+            <ImageGalleryItem
+              key={el.id}
+              onModal={loadModal}
+              id={index}
+              item={el}
+            />
+          ))}
+        </ul>
+        {totalHits > currentPage * 12 && <Button onLoadMore={handleLoadMore} />}
+        {showModal && <Modal item={imageList[id]} onClose={toggleModal} />}
+      </div>
+    );
+  }
+  if (curStatus === STATUS.REJECT) {
+    return <ErrorView message={errorMessage} tryAgain={reloadRequest} />;
+  }
+};
 
-    this.getImages(searchParam, currentPage, true);
-  };
-  render() {
-    const {
-      curStatus,
-      errorMessage,
-      imageList,
-      showModal,
-      id,
-      currentPage,
-      totalHits,
-    } = this.state;
-    if (curStatus === status.IDLE) {
-      return <EmptyView />;
-    }
-    if (curStatus === status.PENDING) {
-      return <Loader />;
-    }
-    if (curStatus === status.RESOLVE) {
-      const classes = showModal
-        ? css.container + ' ' + css.no_scroll
-        : css.container;
-      return (
-        <div className={classes}>
-          <ul className={css.gallery}>
-            {imageList.map((el, index) => (
-              <ImageGalleryItem
-                key={el.id}
-                onModal={this.showModal}
-                id={index}
-                item={el}
-              />
-            ))}
-          </ul>
-          {totalHits > currentPage * 12 && (
-            <Button onLoadMore={this.handleLoadMore} />
-          )}
-          {showModal && (
-            <Modal item={imageList[id]} onClose={this.toggleModal} />
-          )}
-        </div>
-      );
-    }
-    if (curStatus === status.REJECT) {
-      return <ErrorView message={errorMessage} tryAgain={this.reloadRequest} />;
-    }
-  }
-}
 ImageGallery.propTypes = {
   searchParam: PropTypes.string,
 };
